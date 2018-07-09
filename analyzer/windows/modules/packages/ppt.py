eb3ea902b74d097285ef477bf41191398adbcb67
Tomer Frid <tomer@sndbox.com>
2018-07-09 18:40:26 +0300

    rename to pps

--- analyzer/windows/modules/packages/ppt.py
+++ analyzer/windows/modules/packages/ppt.py
@@ -2,9 +2,14 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import os
+import logging
+
 from _winreg import HKEY_CURRENT_USER
 
 from lib.common.abstracts import Package
+
+log = logging.getLogger(__name__)
 
 class PPT(Package):
     """PowerPoint analysis package."""
@@ -75,6 +80,12 @@ class PPT(Package):
     ]
 
     def start(self, path):
+        # Transform to pps/ppsx extention
+        if path.endswith(".ppt") or path.endswith(".pptx"):
+            os.rename(path, path + ".pps")
+            path += ".pps"
+            log.info("Submitted file is using ppt/x extension, added .pps")
+
         powerpoint = self.get_path("Microsoft Office PowerPoint")
         return self.execute(
             powerpoint, args=["/S", path], mode="office",
