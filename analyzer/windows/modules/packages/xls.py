9f2a1a589dc0fe018d2c9751a0ecb6c412d298c4
Itay Hury <itay+github@huri.biz>
2018-11-06 16:49:58 +0200

    Use Office 2010 registry keys

--- analyzer/windows/modules/packages/xls.py
+++ analyzer/windows/modules/packages/xls.py
@@ -24,17 +24,17 @@ class XLS(Package):
     REGKEYS = [
         [
             HKEY_CURRENT_USER,
-            "Software\\Microsoft\\Office\\15.0\\Common\\General",
+            "Software\\Microsoft\\Office\\14.0\\Common\\General",
             {
-                # "Welcome to the 2015 Microsoft Office system"
+                # "Welcome to the 2010 Microsoft Office system"
                 "ShownOptIn": 1,
             },
         ],
         [
             HKEY_CURRENT_USER,
-            "Software\\Microsoft\\Office\\15.0\\Excel\\Security",
+            "Software\\Microsoft\\Office\\14.0\\Excel\\Security",
             {
-                # Enable VBA macros in Office 2015.
+                # Enable VBA macros in Office 2010.
                 "VBAWarnings": 1,
                 "AccessVBOM": 1,
 
