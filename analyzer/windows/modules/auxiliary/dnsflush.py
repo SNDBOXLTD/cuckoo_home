5622be518cc8eb9b23b59b9b1d6040009f113dc8
Tomer Frid <tomer@sndbox.com>
2019-02-25 12:15:41 +0200

    add docs + error type

--- analyzer/windows/modules/auxiliary/dnsflush.py
+++ analyzer/windows/modules/auxiliary/dnsflush.py
@@ -6,17 +6,20 @@ import logging
 
 from lib.api.process import subprocess_checkcall
 from lib.common.abstracts import Auxiliary
-from lib.common.exceptions import CuckooError
 
 log = logging.getLogger(__name__)
 
 
 class DnsFlush(Auxiliary):
-    """Flush DNS Cache."""
+    """Flush DNS Cache.
+
+    Flush DNS cache before analysis start so all requests start with a dns query.
+    This helps accurate the post processing pcap filtering
+    """
 
     def start(self):
         try:
             subprocess_checkcall(["ipconfig.exe", "/flushdns"])
             log.info("Successfully flushed dns.")
-        except CuckooError as e:
+        except Exception as e:
             log.error("Error flushing dns: %s", e)
