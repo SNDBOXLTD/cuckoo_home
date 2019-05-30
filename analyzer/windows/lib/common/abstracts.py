bb0647436f927c0b47fd6262d86e3ef354c6012b
Tomer Frid <tomer@sndbox.com>
2019-05-30 11:47:17 +0300

    verify network connectivity check before start

--- analyzer/windows/lib/common/abstracts.py
+++ analyzer/windows/lib/common/abstracts.py
@@ -6,11 +6,16 @@
 import glob
 import os
 import base64
+import urllib2
+import json
+import logging
 
 from _winreg import CreateKey, SetValueEx, CloseKey, REG_DWORD, REG_SZ
 
 from lib.api.process import Process
 from lib.common.exceptions import CuckooPackageError
+
+log = logging.getLogger(__name__)
 
 class Package(object):
     """Base abstract analysis package."""
@@ -154,6 +159,19 @@ class Package(object):
         with open(adobe_config_path, "a") as file_to_append:
             file_to_append.write(conf_flag)
 
+    @staticmethod
+    def _verify_network():
+        sndbox_api_ep = "https://api.sndbox.com"
+        try:
+            response = urllib2.urlopen(sndbox_api_ep, timeout=5)
+            data = json.load(response)
+            assert(data.get('status') == 'online')
+            return True
+        except:
+            log.exception("Failed to verify network connection.")
+        return False
+
+
     def execute(self, path, args, mode=None, maximize=False, env=None,
                 source=None, trigger=None):
         """Starts an executable for analysis.
@@ -174,6 +192,10 @@ class Package(object):
         log_pipe = self.options.get("forwarderpipe")
         dispatcher_pipe = self.options.get("dispatcherpipe")
         driver_options = self.options.get("driver_options")
+
+        # Check network
+        if not self._verify_network():
+            raise CuckooPackageError("Unable to verify network, analysis aborted.")
 
         # Kernel analysis overrides the free argument.
         if analysis == "kernel":
